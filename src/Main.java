0007ead86ea545bb10b93b137abe864a5f5ad0e0
skylar11d <skylar11d@aol.com>
2024-10-29 17:34:45 +0300

    initial commitment

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import model.Vertix;
+import utilities.BreadthFirstSearch;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Vertix<Integer> v0 = new Vertix<>(0);
+        Vertix<Integer> v1 = new Vertix<>(1);
+        Vertix<Integer> v2 = new Vertix<>(2);
+        Vertix<Integer> v3 = new Vertix<>(3);
+        Vertix<Integer> v4 = new Vertix<>(4);
+        Vertix<Integer> v5 = new Vertix<>(5);
+        Vertix<Integer> v6 = new Vertix<>(6);
+
+        v0.setNeighbors(Arrays.asList(v1, v2));
+        v1.setNeighbors(Arrays.asList(v3));
+        v2.setNeighbors(Arrays.asList(v4));
+        v3.setNeighbors(Arrays.asList(v5));
+        v5.setNeighbors(Arrays.asList(v6));
+        v6.setNeighbors(Arrays.asList(v0));
+        v4.setNeighbors(Arrays.asList(v1));
+
+        //v0 will be our starting vertix or node
+
+        BreadthFirstSearch<Integer> bfs = new BreadthFirstSearch<>(v0);
+
+        bfs.search();
+
+    }
+
+}
